3d92d5009dfca416c510e81cfc021194182f170b
parasaurolophus <apps@rader.us>
2023-01-23 02:29:50 -0600

    Use DOM API in preference to setting innerHTML

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -4,6 +4,29 @@ var ws = null
 var wsReadyStateTimer = null
 var wsReconnectTimer = null
 
+document.addEventListener('DOMContentLoaded', function () {
+
+    document.querySelector('#ws-url').value = wsURL(window.location)
+    connectWS()
+
+})
+
+function removeAllChildren(element) {
+
+    while (element.firstChild) {
+
+        element.removeChild(element.firstChild)
+
+    }
+}
+
+function replaceChildren(element, child) {
+
+    removeAllChildren(element)
+    element.appendChild(child)
+
+}
+
 function wsURL(location) {
 
     const parts = /([^:]+):\/\/([^/]+)(.*)/.exec(location)
@@ -23,63 +46,88 @@ function wsURL(location) {
 
 function createHueKey(event) {
 
-    const msg = {
+    ws.send(JSON.stringify({
         payload: event.target.value,
         topic: 'put/hue/create-key'
-    }
-
-    ws.send(JSON.stringify(msg))
-
+    }))
 }
 
 function hueBridges(bridges) {
 
-    let content = '<table>'
+    const labels = ['id', 'name', 'mode', 'host', 'address', 'port', 'status', '&nbsp;']
+    const table = document.createElement('table')
+    const headingRow = document.createElement('tr')
 
-    content += '<tr>'
-    content += '<th>id</th>'
-    content += '<th>name</th>'
-    content += '<th>model</th>'
-    content += '<th>host</th>'
-    content += '<th>address</th>'
-    content += '<th>port</th>'
-    content += '<th>status</th>'
-    content += '<th>&nbsp;</th>'
-    content += '</tr>'
+    for (let label of labels) {
 
-    for (let bridge of bridges) {
+        const heading = document.createElement('th')
 
-        content += '<tr>'
-        content += '<td>' + bridge.id + '</td>'
-        content += '<td>' + bridge.name + '</td>'
-        content += '<td>' + bridge.model + '</td>'
-        content += '<td>' + bridge.host + '</td>'
-        content += '<td>' + bridge.address + '</td>'
-        content += '<td>' + bridge.port + '</td>'
-        content += '<td class="centered">' + hueStatus(bridge.status) + '</td>'
-        content +=
-            '<td><button onclick="createHueKey(event)" value="' +
-            bridge.address +
-            '">create key</button></td>'
-        content += '</tr>'
+        if (label == '&nbsp;') {
+
+            heading.innerHTML = '&nbsp;'
+
+        } else {
+
+            heading.appendChild(document.createTextNode(label))
+
+        }
+
+        headingRow.appendChild(heading)
 
     }
 
-    content += '</table>'
-    document.querySelector('#hue-bridges').innerHTML = content
+    table.appendChild(headingRow)
+
+    for (let bridge of bridges) {
+
+        const row = document.createElement('tr')
+
+        for (let label of labels) {
+
+            const data = document.createElement('td')
+
+            if (label == 'status') {
+
+                data.className = 'centered'
+                data.appendChild(hueStatus(bridge.status))
+
+            } else if (label == '&nbsp;') {
+
+                const button = document.createElement('button')
+
+                button.setAttribute('value', bridge.address)
+                button.setAttribute('onclick', 'createHueKey(event)')
+                button.appendChild(document.createTextNode('Create Key'))
+                data.appendChild(button)
+
+            } else {
+
+                data.appendChild(document.createTextNode(bridge[label]))
+
+            }
+
+            row.appendChild(data)
+
+        }
+
+        table.appendChild(row)
+
+    }
+
+    const container = document.querySelector('#hue-bridges')
+
+    replaceChildren(container, table)
+    return container
 
 }
 
 function hueGroupChanged(event) {
 
-    const msg = {
+    ws.send(JSON.stringify({
         payload: { on: { on: event.target.checked } },
         topic: event.target.value,
         method: 'PUT'
-    }
-
-    ws.send(JSON.stringify(msg))
-
+    }))
 }
 
 function hueSceneClicked(event) {
@@ -96,120 +144,130 @@ function hueSceneClicked(event) {
 
 function activateWindowShadesScene(event) {
 
-    const msg = { payload: event.target.value, topic: 'put/powerview/scene' }
-
-    ws.send(JSON.stringify(msg))
-
+    ws.send(JSON.stringify({
+        payload: event.target.value,
+        topic: 'put/powerview/scene'
+    }))
 }
 
 function refreshControlsClicked(_event) {
 
-    const msg = {
+    removeAllChildren(document.querySelector('#controls-container'))
+    models = {}
+    ws.send(JSON.stringify({
         payload: new Date().getTime(),
         topic: 'controls/refresh'
-    }
-
-    models = {}
-    ws.send(JSON.stringify(msg))
+    }))
 }
 
 function powerviewSceneButton(scene) {
 
-    return '<button onclick="activateWindowShadesScene(event)" value="' +
-        scene.id +
-        '">' +
-        scene.name +
-        '</button>'
+    const button = document.createElement('button')
+
+    button.setAttribute('onclick', 'activateWindowShadesScene(event)')
+    button.setAttribute('value', scene.id)
+    button.textContent = scene.name
+
+    return button
 
 }
 
 function powerviewRoom(room) {
 
-    let controls = '<fieldset>'
+    const fieldset = document.createElement('fieldset')
+    const legend = document.createElement('legend')
+    const div = document.createElement('div')
 
-    controls += '<legend>' + room.name + '</legend>'
-    controls += '<div class="wrapped">'
+    legend.textContent = room.name
+    div.className = 'wrapped'
+    fieldset.appendChild(legend)
+    fieldset.appendChild(div)
 
     for (let scene of room.scenes) {
 
-        controls += powerviewSceneButton(scene)
+        fieldset.appendChild(powerviewSceneButton(scene))
 
     }
 
-    controls += '</div></fieldset>'
-    return controls
+    return fieldset
 
 }
 
 function powerviewControls(msg) {
 
-    let controls = '<section class="padded"><h3>Window Shades</h3>'
+    const section = document.createElement('section')
+    const heading = document.createElement('h3')
 
-    if (msg.info) {
-
-        controls +=
-            '<div class="info">' +
-            msg.info +
-            '</div>'
-
-    }
+    heading.textContent = 'Window Shades'
+    section.appendChild(heading)
+    section.className = 'padded'
+    appendInfo(section, msg)
 
     for (let room of msg.payload) {
 
-        controls += powerviewRoom(room)
+        section.appendChild(powerviewRoom(room))
 
     }
 
-    controls += '</section>'
-    return controls
+    return section
 
 }
 
 function hueGroupCheckbox(group) {
 
-    let controls = '<input type="checkbox" id="' +
-        group.grouped_light.id +
-        '" value="' +
-        group.value +
-        '" ' +
-        (group.grouped_light.on.on ? 'checked' : '') +
-        ' onchange="hueGroupChanged(event)" />'
+    const label = document.createElement('label')
+    const input = document.createElement('input')
+    const text = document.createTextNode('On')
 
-    controls += '<label for="' + group.grouped_light.id + '">On</label>'
+    input.id = group.grouped_light.id
+    input.setAttribute('type', 'checkbox')
+    input.setAttribute('value', group.value)
+    input.setAttribute('onchange', 'hueGroupChanged(event)')
 
-    return controls
+    if (group.grouped_light.on.on) {
+
+        input.setAttribute('checked', true)
+
+    }
+
+    label.className = 'padded'
+    label.appendChild(input)
+    label.appendChild(text)
+    return label
 
 }
 
 function hueSceneButton(scene) {
 
-    return '<button onclick="hueSceneClicked(event)" id="' +
-        scene.id +
-        '" value="' +
-        scene.value +
-        '">' +
-        scene.metadata.name +
-        '</button>'
+    const button = document.createElement('button')
+
+    button.id = scene.id
+    button.setAttribute('value', scene.value)
+    button.setAttribute('onclick', 'hueSceneClicked(event)')
+    button.textContent = scene.metadata.name
+    return button
 
 }
 
 function hueGroup(group) {
 
-    let controls = '<fieldset>'
+    const fieldset = document.createElement('fieldset')
+    const legend = document.createElement('legend')
+    const div = document.createElement('div')
 
-    controls += '<legend>' + group.name + '</legend>'
-    controls += hueGroupCheckbox(group)
-    controls += '<div class="wrapped">'
-
+    div.className = 'wrapped'
+    legend.textContent = group.name
+    fieldset.appendChild(legend)
+    fieldset.appendChild(hueGroupCheckbox(group))
 
     for (let scene of group.scenes) {
 
-        controls += hueSceneButton(scene)
+        div.appendChild(hueSceneButton(scene))
 
     }
 
-    controls += '</div></fieldset>'
-    return controls
+    fieldset.appendChild(div)
+    return fieldset
 
 }
 
@@ -217,103 +275,90 @@ function hueControls(msg) {
 
     const model = msg.payload
 
-    let controls =
-        '<section class="padded"><h3>' +
-        model.title +
-        '</h3>'
+    const section = document.createElement('section')
+    const heading = document.createElement('h3')
 
-    if (msg.info) {
-
-        controls +=
-            '<div class="info">' +
-            msg.info +
-            '</div>'
-
-    }
+    section.className = 'padded'
+    heading.textContent = model.title
+    section.appendChild(heading)
+    appendInfo(section, msg)
 
     for (let group of model.groups) {
 
-        controls += hueGroup(group)
+        section.appendChild(hueGroup(group))
 
     }
 
-    controls += '</section>'
-    return controls
+    return section
 
+}
+
+function appendInfo(section, msg) {
+
+    if (msg.info) {
+
+        const info = document.createElement('div')
+
+        info.className = 'info'
+        info.textContent = msg.info
+        section.appendChild(info)
+
+    }
 }
 
 function settingsCheckboxChanged(event) {
 
-    const msg = {
+    ws.send(JSON.stringify({
         payload: event.target.checked,
         topic: event.target.id.replace('-', '/'),
         retain: true
-    }
-
-    ws.send(JSON.stringify(msg))
+    }))
 
 }
 
 function bedtimeChanged(event) {
 
-    const msg = {
+    ws.send(JSON.stringify({
         payload: event.target.value,
         topic: 'settings/bedtime',
         retain: true
-    }
-
-    ws.send(JSON.stringify(msg))
+    }))
 
 }
 
 function wsReadyState() {
 
-    let readyState = ws.readyState
+    const readyState = ws.readyState
 
-    let text =
+    const text =
         readyState == 0 ? 'connecting' :
             readyState == 1 ? 'connected' :
                 readyState == 2 ? 'disconnecting' :
                     readyState == 3 ? 'disconnected' :
                         readyState
 
-    let content =
-        '<span class="padded ws-' +
-        text +
-        '">' +
-        text +
-        '</span>'
+    const span = document.createElement('span')
 
-    document.querySelector("#ws-status").innerHTML = content
+    span.className = 'padded ws-' + text
+    span.appendChild(document.createTextNode(text))
+    replaceChildren(document.querySelector("#ws-status"), span)
 
 }
 
 function hueStatus(status) {
 
-    let text =
+    const text =
         status == -1 ? 'uninitialized' :
             status == 0 ? 'connecting' :
                 status == 1 ? 'connected' :
                     status == 2 ? 'disconnected' :
                         status
 
-    return '<div class="padded hue-' +
-        text +
-        '">' +
-        text +
-        '</div>'
+    const div = document.createElement('div')
 
-
-}
-
-function escapeHTML(unescaped) {
-
-    let escaped = unescaped.replace('&', '&amp;')
-
-    escaped = escaped.replace('<', '&lt;')
-    escaped = escaped.replace('>', '&gt;')
-    escaped = escaped.replace('"', '&quot;')
-    return escaped
+    div.className = 'padded hue-' + text
+    div.appendChild(document.createTextNode(text))
+    return div
 
 }
 
@@ -386,10 +431,18 @@ function connectWS(_event) {
         if (msg.topic == 'automation/trigger') {
 
             msg.payload.timestamp = new Date(msg.payload.timestamp).toLocaleString()
-            document.querySelector('#automation-trigger').innerHTML =
-                '<div><div>Automation trigger:</div><pre>' +
-                escapeHTML(JSON.stringify(msg.payload, undefined, 1)) +
-                '</pre></div>'
+
+            const container = document.querySelector('#automation-trigger')
+            const div1 = document.createElement('div')
+            const div2 = document.createElement('div')
+            const pre = document.createElement('pre')
+
+            pre.textContent = JSON.stringify(msg.payload, undefined, 1)
+            div2.appendChild(pre)
+            div1.appendChild(document.createTextNode('Automation trigger:'))
+            div1.appendChild(div2)
+
+            replaceChildren(container, div1)
             return
 
         }
@@ -446,16 +499,21 @@ function connectWS(_event) {
 
             hueKeys[msg.bridgeAddress] = msg.payload
 
-            let content = '<dl>'
+            const dl = document.createElement('dl')
 
             for (let address in hueKeys) {
 
-                content += '<dt>' + address + '</dt>'
-                content += '<dd>' + hueKeys[address] + '</dd>'
+                const dt = document.createElement('dt')
+                const dd = document.createElement('dd')
+
+                dt.appendChild(document.createTextNode(address))
+                dd.appendChild(document.createTextNode(hueKeys[address]))
+                dl.appendChild(dt)
+                dl.appendChild(dd)
 
             }
 
-            document.querySelector('#hue-key').innerHTML = content
+            replaceChildren(document.querySelector('#hue-key'), dl)
             return
 
         }
@@ -489,22 +547,14 @@ function connectWS(_event) {
 
         }
 
-        let content = ''
+        const container = document.querySelector('#controls-container')
+
+        removeAllChildren(container)
 
         for (let label in models) {
 
-            content += models[label]
+            container.appendChild(models[label])
 
         }
-
-        document.querySelector('#controls-container').innerHTML = content
-
     }
 }
-
-document.addEventListener('DOMContentLoaded', function () {
-
-    document.querySelector('#ws-url').value = wsURL(window.location)
-    connectWS()
-
-})
