ac5c6cac953de954cc3c50bc790a218ed96e880c
parasaurolophus <apps@rader.us>
2023-03-07 16:48:42 -0600

    Use v-switch instead of v-btn to toggle theme

--- dashboard/src/layouts/default/AppBar.vue
+++ dashboard/src/layouts/default/AppBar.vue
@@ -1,24 +1,35 @@
 <template>
     <v-app-bar>
-        <v-app-bar-title>Dashboard </v-app-bar-title>
-        <v-btn @click="toggleTheme">{{ theme.global.current.value.dark ? 'light' : 'dark' }}</v-btn>
+        <v-app-bar-title>Dashboard</v-app-bar-title>
+        <div class="app-bar-switch">
+            <v-switch v-model="selectedTheme" :label="selectedTheme" false-value="dark" true-value="light"
+                @change="setTheme" />
+        </div>
     </v-app-bar>
 </template>
 
 <style scoped>
-/* */
+.app-bar-switch {
+    height: 100%;
+    display: flex;
+    align-content: center;
+}
+
+.app-bar-switch>* {
+    margin: 0 1rem 0 0;
+}
 </style>
 
 <script setup>
+import { ref } from 'vue'
 import { useTheme } from 'vuetify'
 
 const theme = useTheme()
+const selectedTheme = ref('dark')
 
-function toggleTheme() {
+function setTheme() {
 
-    const newTheme = theme.global.current.value.dark ? 'light' : 'dark'
-
-    theme.global.name.value = newTheme
+    theme.global.name.value = selectedTheme.value
 
 }
 </script>
