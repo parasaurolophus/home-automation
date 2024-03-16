0c0687295962e31c9573646e43e48bf6c9f81cba
parasaurolophus <apps@rader.us>
2024-03-16 16:40:52 -0500

    Sort timer debugging values on dashboard

--- dashboard/src/components/dashboard/TimerComponent.vue
+++ dashboard/src/components/dashboard/TimerComponent.vue
@@ -2,9 +2,9 @@
     <fieldset>
         <legend>Timer</legend>
         <table>
-            <tr v-for="(value, name) in timer" :key="name">
-                <th>{{ name }}</th>
-                <td>{{ value }}</td>
+            <tr v-for="(pair, index) in sorted(timer)" :key="index">
+                <th>{{ pair[0] }}</th>
+                <td>{{ pair[1] }}</td>
             </tr>
         </table>
     </fieldset>
@@ -18,4 +18,24 @@
 import { inject } from 'vue'
 
 const timer = inject('timer')
+
+function sorted(obj) {
+    const result = []
+    for (let key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            result.push([key, obj[key]])
+        }
+    }
+    return result.sort((a, b) => {
+        const v1 = a[1]
+        const v2 = b[1]
+        if (v1 == v2) {
+            return 0
+        }
+        if (v1 < v2) {
+            return -1
+        }
+        return 1
+    })
+}
 </script>
