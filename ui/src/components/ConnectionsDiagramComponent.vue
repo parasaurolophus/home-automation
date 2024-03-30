4375fdea23504a0a675a9a81323222149be2506a
parasaurolophus <apps@rader.us>
2024-03-30 18:02:31 -0500

    Simplify ConnectionsDiagramComponent.vue

--- ui/src/components/ConnectionsDiagramComponent.vue
+++ ui/src/components/ConnectionsDiagramComponent.vue
@@ -11,124 +11,87 @@ const theme = useTheme()
 
 const diagram = ref(null)
 
-const websocketPublish = inject('websocketPublish')
-const websocketStatus = inject('websocketStatus')
-const hueBridges = inject('hueBridges')
 const hueModel = inject('hueModel')
 const powerviewModel = inject('powerviewModel')
 const powerviewStatus = inject('powerviewStatus')
 const showAlert = inject('showAlert')
+const websocketPublish = inject('websocketPublish')
+const websocketStatus = inject('websocketStatus')
 
-function findHueBridge(address) {
-
-    for (let bridge of hueBridges.value) {
-
-        if (bridge.address == address) {
-
-            return bridge
-
-        }
-    }
-
-    return undefined
-
-}
-
-function findHueBridgeModel(address) {
-
-    for (let bridge of hueModel.value) {
-
-        if (bridge.id == address) {
-            return bridge
-        }
-    }
-
-    const message = 'no model found for hue bridge with address ' + address
-    console.log(message)
-    showAlert('warning', 'hue bridge metadata', message)
-    return undefined
-}
-
-///////////////////////////////////////////////////////////////////////////////
-
-function renderDiagram() {
-
+function buildDiagram() {
     const themeName = (theme.global.current.value.dark ? '"dark"' : '"light"')
     const grayColor = (theme.global.current.value.dark ? '#888888' : '#888888')
     const redColor = (theme.global.current.value.dark ? '#880000' : '#ff0000')
     const greenColor = (theme.global.current.value.dark ? '#008800' : '#00ff00')
     const yellowColor = (theme.global.current.value.dark ? '#888800' : '#ffff00')
-
-    let flowchart = ''
-
-    flowchart += 'flowchart TB\n\n'
-
-    flowchart += '  %%%%{init: { '
-    flowchart += '"theme": ' + themeName + ', '
-    flowchart += '"flowchart": { "htmlLabels": true, "useMaxWidth": true }'
-    flowchart += ' } }%%%%\n\n'
-
-    flowchart += '  classDef gray fill:' + grayColor + ',stroke:' + grayColor + ',stroke-width:1px\n'
-    flowchart += '  classDef red fill:' + redColor + ',stroke:' + redColor + ',stroke-width:1px\n'
-    flowchart += '  classDef green fill:' + greenColor + ',stroke:' + greenColor + ',stroke-width:1px\n'
-    flowchart += '  classDef yellow fill:' + yellowColor + ',stroke:' + yellowColor + ',stroke-width:1px\n\n'
-
-    const flowsStatus =
+    const flowStatus =
         websocketStatus.value == 0 ? 'connecting' :
             websocketStatus.value == 1 ? 'connected' :
                 websocketStatus.value == 2 ? 'disconnected' :
                     websocketStatus.value
-
-    const flowsClassName = websocketStatus.value == 0 ? 'yellow' : websocketStatus.value == 1 ? 'green' : 'red'
-
-    flowchart += '  ui["ui&nbsp;"]\n'
-    flowchart += '  flows["flows&nbsp;\n(' + flowsStatus + ')&nbsp;"]\n'
-    flowchart += '  class flows ' + flowsClassName + '\n'
-    flowchart += '  click flows call flowsNodeClicked()\n\n'
-
-    flowchart += 'subgraph "LAN&nbsp;"\n\n'
-
-    flowchart += '    subgraph "Node-RED&nbsp;"\n'
-    flowchart += '      ui --- flows\n'
-    flowchart += '      class ui green\n'
-    flowchart += '      click ui call uiNodeClicked()\n'
-    flowchart += '    end\n\n'
-
-    let index = 0
-
-    for (let bridge of hueBridges.value) {
-
-        const bridgeStatus = bridge.status == -1 ? 'uninitialized' : bridge.status == 0 ? 'connecting' : bridge.status == 1 ? 'connected' : 'disconnected'
-        const bridgeClassName = bridge.status == -1 ? 'gray' : bridge.status == 0 ? 'yellow' : bridge.status == 1 ? 'green' : 'red'
-        const bridgeNodeName = 'hue' + ++index
-        const bridgeLabel =
-            '["Hue ' + bridge.address + '&nbsp;\n' +
-            '(' + bridgeStatus + ')&nbsp;"]'
-
-        flowchart += bridgeNodeName + bridgeLabel + '\n'
-        flowchart += '    class ' + bridgeNodeName + ' ' + bridgeClassName + '\n\n'
-        flowchart += '    click ' + bridgeNodeName + ' call hueBridgeNodeClicked("' + bridge.address + '")\n'
-        flowchart += '    flows --- ' + bridgeNodeName + '\n\n'
-
+    const flowClassName = websocketStatus.value == 0 ? 'yellow' :
+        websocketStatus.value == 1 ? 'green' :
+            'red'
+    let flowchart = ''
+    flowchart += 'flowchart TB\n'
+    flowchart += '    %%%%{init: { "theme": ' + themeName + ', "flowchart": { "htmlLabels": true, "useMaxWidth": true } } }%%%%\n'
+    flowchart += '    classDef gray fill:' + grayColor + ',stroke:' + grayColor + ',stroke-width:1px\n'
+    flowchart += '    classDef red fill:' + redColor + ',stroke:' + redColor + ',stroke-width:1px\n'
+    flowchart += '    classDef green fill:' + greenColor + ',stroke:' + greenColor + ',stroke-width:1px\n'
+    flowchart += '    classDef yellow fill:' + yellowColor + ',stroke:' + yellowColor + ',stroke-width:1px\n'
+    flowchart += '    subgraph LAN\n'
+    flowchart += '        subgraph Node-RED\n'
+    flowchart += '            ui["Vuetify-based UI"]\n'
+    flowchart += '            class ui green\n'
+    flowchart += '            click ui call uiNodeClicked()\n'
+    flowchart += '            flow["Node-RED Flow\n(' + flowStatus + ')"]\n'
+    flowchart += '            class flow ' + flowClassName + '\n'
+    flowchart += '            click flow call flowNodeClicked()\n'
+    flowchart += '            ui <-- WebSocket --> flow\n'
+    flowchart += '        end\n'
+    let counter = 0
+    for (let model of hueModel.value) {
+        const bridgeStatus = model.status == -1 ? 'uninitialized' :
+            model.status == 0 ? 'connecting' :
+                model.status == 1 ? 'connected' :
+                    'disconnected'
+        const bridgeClassName = model.status == -1 ? 'gray' :
+            model.status == 0 ? 'yellow' :
+                model.status == 1 ? 'green' :
+                    'red'
+        const bridgeName = 'hue_bridge' + counter
+        const lightsName = 'hue_lights' + counter
+        flowchart += '        ' + bridgeName + '["' + model.title + ' Hue Bridge\n(' + bridgeStatus + ')"]\n'
+        flowchart += '        class ' + bridgeName + ' ' + bridgeClassName + '\n'
+        flowchart += '        click ' + bridgeName + ' call hueBridgeNodeClicked("' + model.id + '")\n'
+        flowchart += '        ' + lightsName + '([Hue Lights])\n'
+        flowchart += '        flow <-- WiFi --> ' + bridgeName + '\n'
+        flowchart += '        ' + bridgeName + '<-- Zigbee --> ' + lightsName + '\n'
+        counter += 1
     }
-
     if (powerviewModel.value.length > 0) {
-
-        const className = powerviewStatus.value === 0 ? 'yellow' : powerviewStatus.value === 1 ? 'green' : 'red'
-        flowchart += '    flows --- powerview["PowerView&nbsp;"]\n'
-        flowchart += '    class powerview ' + className + '\n'
-        flowchart += '    click powerview call powerviewHubNodeClicked()\n\n'
-
+        const powerviewClassName = powerviewStatus.value === 0 ? 'yellow' :
+            powerviewStatus.value === 1 ? 'green' :
+                'red'
+        const hubStatus = powerviewStatus.value == 0 ? 'unknown' :
+            powerviewStatus.value == 1 ? 'connected' :
+                'error'
+        flowchart += '        powerview_hub["PowerView Hub\n(' + hubStatus + ')"]\n'
+        flowchart += '        class powerview_hub ' + powerviewClassName + '\n'
+        flowchart += '        powerview_shades([PowerView Shades])\n'
+        flowchart += '        flow <-- WiFi --> powerview_hub\n'
+        flowchart += '        powerview_hub <-- Bluetooth --> powerview_shades\n'
+        flowchart += '        click powerview_hub call powerviewHubNodeClicked()\n'
     }
-
-    flowchart += '  end\n'
+    flowchart += '    end\n'
     return flowchart
-
 }
 
-async function drawDiagram() {
+async function renderMermaid() {
 
-    const { svg, bindFunctions } = await mermaid.render('ui', renderDiagram())
+    const flowchart = buildDiagram()
+    console.log(flowchart)
+    const { svg, bindFunctions } = await mermaid.render('ui', flowchart)
 
     diagram.value.innerHTML = svg
     bindFunctions?.(diagram.value)
@@ -136,38 +99,44 @@ async function drawDiagram() {
 
 mermaid.flowchartConfig = { width: '100%' }
 mermaid.initialize({ startOnLoad: false, securityLevel: 'loose' })
-onMounted(drawDiagram)
-watch(websocketStatus, drawDiagram)
-watch(hueBridges, drawDiagram)
-watch(theme.global.current, drawDiagram)
-watch(powerviewStatus, drawDiagram)
+onMounted(renderMermaid)
+watch(hueModel, renderMermaid)
+watch(powerviewModel, renderMermaid)
+watch(websocketStatus, renderMermaid)
+watch(powerviewStatus, renderMermaid)
 
 ////////////////////////////////////////////////////////////////////////////////
 // TO DO: investigate ways to have the mermaid diagram invoke these directly
 // rather than using indirection through index.html
 ////////////////////////////////////////////////////////////////////////////////
 
+function findHueModel(address) {
+
+    for (let model of hueModel.value) {
+
+        if (model.id == address) {
+            return model
+        }
+    }
+    return undefined
+}
+
 // eslint-disable-next-line no-global-assign, no-undef
 hueBridgeNodeClicked = (address) => {
-    const bridge = findHueBridge(address)
-    if (bridge) {
-        const text = JSON.stringify(bridge, undefined, 4)
-        const model = findHueBridgeModel(address)
-        if (model) {
-            showAlert('info', model.title, text)
-            return
-        }
-        showAlert('warning', 'hue bridge ' + address, text)
+    const model = findHueModel(address)
+    if (model) {
+        const text = JSON.stringify(model, undefined, 4)
+        showAlert('info', model.title, text)
         return
     }
-    showAlert('warning', 'hue bridge not found', 'no metadata found for hue bridge with address ' + address)
+    showAlert('warning', 'hue bridge ' + address, 'no model found for ' + address)
 }
 
 // eslint-disable-next-line no-global-assign, no-undef
 powerviewHubNodeClicked = () => showAlert('info', 'powerview hub', JSON.stringify(powerviewModel.value, undefined, 4))
 
 // eslint-disable-next-line no-global-assign, no-undef
-flowsNodeClicked = () => websocketPublish({ payload: new Date().getTime(), topic: 'controls/refresh' })
+flowNodeClicked = () => websocketPublish({ payload: new Date().getTime(), topic: 'controls/refresh' })
 
 // eslint-disable-next-line no-global-assign, no-undef
 uiNodeClicked = () => alert('ui node clicked')
