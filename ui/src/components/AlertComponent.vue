301d04400e399c0e2cbc8d4ebe8d65fe0cb5543d
parasaurolophus <apps@rader.us>
2024-03-30 11:35:19 -0500

    Add "close all" button when multiple alerts are shown

--- ui/src/components/AlertComponent.vue
+++ ui/src/components/AlertComponent.vue
@@ -1,10 +1,21 @@
 <template>
-    <div v-for="(alert, index) in alerts" :key="index">
-        <v-alert v-model="alert.show" :type="alert.type" closable @update:modelValue="closeAlert">
-            <template #title>{{ alert.title }}</template>
-            <pre>{{ alert.text }}</pre>
-        </v-alert>
-    </div>
+    <v-container>
+        <v-row v-if="alerts.length > 1">
+            <v-col>
+                <v-btn @click="closeAll">
+                    Close All
+                </v-btn>
+            </v-col>
+        </v-row>
+    </v-container>
+    <v-row v-for="(alert, index) in alerts" :key="index">
+        <v-col>
+            <v-alert v-model="alert.show" :type="alert.type" closable @update:modelValue="closeAlert">
+                <template #title>{{ alert.title }}</template>
+                <pre>{{ alert.text }}</pre>
+            </v-alert>
+        </v-col>
+    </v-row>
 </template>
 
 <script setup>
@@ -13,8 +24,13 @@ import { inject } from 'vue'
 const alerts = inject('alerts')
 
 function closeAlert() {
-
     alerts.value = alerts.value.filter(alert => alert.show)
+}
 
+function closeAll() {
+    for (let alert of alerts.value) {
+        alert.show = false
+    }
+    closeAlert()
 }
 </script>
