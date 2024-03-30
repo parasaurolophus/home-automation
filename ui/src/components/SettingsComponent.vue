5917734af9d8c6eddae8f2c5498da384663c9284
parasaurolophus <apps@rader.us>
2024-03-30 11:12:24 -0500

    Improve layout of settings panel in ui

--- ui/src/components/SettingsComponent.vue
+++ ui/src/components/SettingsComponent.vue
@@ -2,54 +2,58 @@
     <v-container>
         <v-row>
             <v-col>
-                <div>
-                    <v-switch v-model="settingsLighting">
-                        <template #label>
-                            <v-icon icon="mdi-lightbulb-group"
-                                :color="settingsLighting ? 'secondary' : 'primary'"></v-icon>
-                        </template>
-                    </v-switch>
-                </div>
-                <div>
-                    Lighting automation
-                    <span v-if="settingsLighting" :style="'color:' + secondaryColor">enabled</span>
-                    <span v-else :style="'color:' + primaryColor">disabled</span>
-                </div>
+                <v-switch v-model="settingsLighting">
+                    <template #prepend>
+                        <v-icon icon="mdi-lightbulb-group" :color="settingsLighting ? 'secondary' : 'primary'"></v-icon>
+                    </template>
+                </v-switch>
+                Lighting automation
+                <span v-if="settingsLighting" :style="'color:' + secondaryColor">enabled</span>
+                <span v-else :style="'color:' + primaryColor">disabled</span>
             </v-col>
-            <v-col>
-                <div>
-                    <v-switch v-model="settingsShades">
-                        <template #label>
-                            <v-icon icon="mdi-blinds" :color="settingsShades ? 'secondary' : 'primary'"></v-icon>
-                        </template>
-                    </v-switch>
-                    <div>
-                        Shades automation
-                        <span v-if="settingsShades" :style="'color:' + secondaryColor">enabled</span>
-                        <span v-else :style="'color:' + primaryColor">disabled</span>
-                    </div>
-                </div>
-            </v-col>
-            <v-col>
-                <div>
-                    <v-btn-toggle mandatory v-model="bedtimeToggle">
-                        <v-btn v-for="(item, index) in bedtimeOptions" :key="index">
-                            {{ item.label }}
-                        </v-btn>
-                    </v-btn-toggle>
-                </div>
+            <v-col class="align-center">
+                <v-btn-toggle mandatory v-model="bedtimeToggle">
+                    <v-btn v-for="(item, index) in bedtimeOptions" :key="index">
+                        {{ item.label }}
+                    </v-btn>
+                </v-btn-toggle>
                 <div :style="'color:' + secondaryColor + ';'">
                     {{ new Date(currentBedtime).toLocaleString() }}
                 </div>
-                <div>
-                    Bedtime automation will be triggered at a randomly chosen time each day that is up to 30 minutes
-                    before or after
-                    the hour selected here
-                </div>
+                Bedtime automation will be triggered at a randomly chosen time each day that is up to 30 minutes before
+                or after the
+                hour selected here
+            </v-col>
+            <v-col class="align-right">
+                <v-switch v-model="settingsShades" class="align-right">
+                    <template #append>
+                        <v-icon icon="mdi-blinds" :color="settingsShades ? 'secondary' : 'primary'">
+                        </v-icon>
+                    </template>
+                </v-switch>
+                Shades automation
+                <span v-if="settingsShades" :style="'color:' + secondaryColor">enabled</span>
+                <span v-else :style="'color:' + primaryColor">disabled</span>
             </v-col>
         </v-row>
     </v-container>
 </template>
+
+<style scoped>
+.align-right {
+    justify-self: right;
+    justify-content: right;
+    justify-items: right;
+    text-align: right;
+}
+
+.align-center {
+    justify-self: center;
+    justify-content: center;
+    justify-items: center;
+    text-align: center;
+}
+</style>
 
 <script setup>
 import { inject, ref, watch } from 'vue'
