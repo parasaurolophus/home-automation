28e18132a6f3415c375cfe18554a6a8565d641ca
parasaurolophus <apps@rader.us>
2024-04-01 12:06:28 -0500

    Add app bar extension containing status text

--- ui/src/layouts/default/AppBar.vue
+++ ui/src/layouts/default/AppBar.vue
@@ -1,6 +1,37 @@
 <template>
-    <v-app-bar class="app-bar">
+    <v-app-bar class="app-bar" extension-height="96">
         <v-app-bar-title>Home Automation</v-app-bar-title>
+        <template #extension>
+            <v-container>
+                <v-row>
+                    <v-col>
+                        <v-card>
+                            <v-card-title>Bedtime</v-card-title>
+                            <v-card-text>
+                                {{ timerString('bedtime') }}
+                            </v-card-text>
+                        </v-card>
+                    </v-col>
+                    <v-col>
+                        <v-card>
+                            <v-card-title>Lighting Automation</v-card-title>
+                            <v-card-text>
+                                {{ settingsLighting ? 'enabled' : 'disabled' }}
+                            </v-card-text>
+                        </v-card>
+                    </v-col>
+                    <v-col>
+                        <v-card>
+                            <v-card-title>Shades Automation</v-card-title>
+                            <v-card-text>
+                                {{ settingsShades ? 'enabled' : 'disabled' }}
+                            </v-card-text>
+                        </v-card>
+                    </v-col>
+                    <v-col><v-spacer /></v-col>
+                </v-row>
+            </v-container>
+        </template>
         <v-btn-toggle mandatory v-model="bedtimeIndex">
             <v-btn v-for="(item, index) in bedtimeOptions" :key="index"
                 @click="websocketPublish({ payload: bedtimeOptions[index].hour, topic: 'settings/bedtime', retain: true, label: 'user' })">
@@ -9,7 +40,7 @@
         </v-btn-toggle>
         <div class="align-center">
             <v-switch v-model="settingsLighting" class="align-center">
-                <template #label>
+                <template #prepend>
                     <v-icon v-if="settingsLighting" color="secondary" icon="mdi-lightbulb-on" />
                     <v-icon v-else color="primary" icon="mdi-lightbulb" />
                 </template>
@@ -17,7 +48,7 @@
         </div>
         <div class="align-center">
             <v-switch v-model="settingsShades" class="align-center">
-                <template #label>
+                <template #prepend>
                     <v-icon v-if="settingsShades" color="secondary" icon="mdi-blinds-open" />
                     <v-icon v-else color="primary" icon="mdi-blinds" />
                 </template>
@@ -51,6 +82,7 @@ const bedtimeOptions = inject('bedtimeOptions')
 const settingsBedtime = inject('settingsBedtime')
 const settingsLighting = inject('settingsLighting')
 const settingsShades = inject('settingsShades')
+const timerTime = inject('timerTime')
 const websocketPublish = inject('websocketPublish')
 
 const selectedTheme = ref(theme.global.name.value == 'dark' ? 0 : 1)
@@ -67,5 +99,12 @@ function selectedBedtimeHour() {
     const bedtime = settingsBedtime.value
     const selected = bedtime.hour - option.hour
     bedtimeIndex.value = selected
+}
+
+function timerString(name) {
+    if (Object.prototype.hasOwnProperty.call(timerTime.value, name)) {
+        return new Date(timerTime.value[name]).toLocaleString()
+    }
+    return 'unknown'
 }
 </script>
--- ui/src/views/Home.vue
+++ ui/src/views/Home.vue
@@ -3,33 +3,6 @@
 
     <AlertComponent />
 
-    <v-container>
-        <v-row>
-            <v-col>
-                <v-card>
-                    <v-card-title>Bedtime</v-card-title>
-                    <v-card-text>
-                        {{ timerString('bedtime') }}
-                    </v-card-text>
-                </v-card>
-            </v-col>
-            <v-col><v-spacer /></v-col>
-            <v-col>
-                <v-card-title>Lighting Automation</v-card-title>
-                <v-card-text>
-                    {{ settingsLighting ? 'enabled' : 'disabled' }}
-                </v-card-text>
-            </v-col>
-            <v-col><v-spacer /></v-col>
-            <v-col>
-                <v-card-title>Shades Automation</v-card-title>
-                <v-card-text>
-                    {{ settingsBedtime ? 'enabled' : 'disabled' }}
-                </v-card-text>
-            </v-col>
-        </v-row>
-    </v-container>
-
     <v-expansion-panels>
 
         <v-expansion-panel title="Lighting" value="lighting">
@@ -55,20 +28,8 @@
 </template>
 
 <script setup>
-import { inject } from 'vue'
 import AlertComponent from '@/components/AlertComponent.vue'
 import StatusComponent from '@/components/StatusComponent.vue'
 import HueControlsComponent from '@/components/HueControlsComponent.vue'
 import PowerViewControlsComponent from '@/components/PowerViewControlsComponent.vue'
-
-const timerTime = inject('timerTime')
-const settingsLighting = inject('settingsLighting')
-const settingsBedtime = inject('settingsBedtime')
-
-function timerString(name) {
-    if (Object.prototype.hasOwnProperty.call(timerTime.value, name)) {
-        return new Date(timerTime.value[name]).toLocaleString()
-    }
-    return 'unknown'
-}
 </script>
