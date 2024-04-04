3661e862e43464dc1ff3393d84f8c0774037dc5e
parasaurolophus <apps@rader.us>
2024-04-04 14:16:13 -0500

    Remove app bar extension from UI

--- ui/src/components/HomeAutomationAppBar.vue
+++ ui/src/components/HomeAutomationAppBar.vue
@@ -1,20 +1,9 @@
 <template>
 
-    <v-app-bar class="app-bar" title="Home Automation" extended>
+    <v-app-bar class="app-bar" title="Home Automation">
         <template #prepend>
             <v-app-bar-nav-icon @click.stop="displayMenu = !displayMenu">
             </v-app-bar-nav-icon>
-        </template>
-        <template #extension>
-            <v-spacer />
-            <v-chip v-if="automationTrigger" color="secondary">
-                {{ automationTrigger['timer/time'] ?? 'no time specified' }}
-                {{ new Date(automationTrigger.at).toLocaleString() }}
-            </v-chip>
-            <v-chip v-if="nextTrigger" color="primary">
-                {{ nextTrigger.label }}
-                {{ new Date(nextTrigger.time).toLocaleString() }}
-            </v-chip>
         </template>
         <v-icon :color="settingsLighting ? 'primary' : 'secondary'">
             {{ settingsLighting ? 'mdi-lightbulb-on' : 'mdi-lightbulb' }}
@@ -40,8 +29,6 @@
 <script setup>
 import { inject, ref } from 'vue'
 
-const automationTrigger = inject('automationTrigger')
-const nextTrigger = inject('nextTrigger')
 const settingsLighting = inject('settingsLighting')
 const settingsShades = inject('settingsShades')
 const standardTimerThemeIcon = inject('standardTimerThemeIcon')
